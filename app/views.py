5cfe46bd13b6865f9cdda855444960bdeda25781
Matt Parrilla <matthew.parrilla@gmail.com>
2014-01-08 15:34:21 -0500

    Add new images

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ def index():
     page_url = BASE_URL + request.path
     social = {
         'title': "Dorothy's List",
-        'subtitle': 'A montly read-along with VPR for young adults',
-        'img': 'http://www.vpr.net/apps/dorothy/static/img/dec-book.jpg',
-        'description': "Dorothy's List is a monthly series by VPR based on the Dorothy Canfield Fisher reading list",
-        'twitter_text': "Read along with Dorothy's List, a VPR series",
-        'twitter_hashtag': 'reading'
+        'subtitle': "VPR's Book Club For Kids",
+        'img': 'http://www.vpr.net/apps/dorothys-list/static/img/wonder.jpg',
+        'description': "Each month VPR highlights a book nominated for the Dorothy Canfield Fisher Children's Book Award. This month's book is 'Wonder' by R.J. Palacio.",
+        'twitter_text': "Read along with Dorothy's List, a VPR book club for kids:",
+        'twitter_hashtag': 'books,reading'
     }
 
     return render_template('content.html', social=social, page_url=page_url)
